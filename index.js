7a878c88601027f650a258a124c0d14ccb708e3a
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-01-16 00:20:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-var animal = 'dog'
 
-function myAnimal() {
-  return animal
-}
-
-function yourAnimal() {
-var animal = 'cat';
-  return animal
-}
-
-function add2(n) {
-  var two= 2
-  return n + two;
-}
-
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
-}
-
-var funkyFunction = function() {
-    return function(){
-      return "FUNKY!"
-    }
-  }
-
-   var theFunk = funkyFunction()()// write your code below!
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
